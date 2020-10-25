f2a64dbbf76738b871bd0778dffe5878a7bcec51
Tobias Guse <tobe.guse@gmail.com>
2020-10-25 21:13:30 +0100

    fetching actual data

--- src/components/Sku.vue
+++ src/components/Sku.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Sku {{name}}</h1>
+  <div v-if='sku && sku !== "invalid"'>
+    <h2>{{sku.current_variant.name}} {{sku.current_variant.display_attributes.color}}</h2>
+    <p>{{sku.current_variant.display_attributes.description}}</p>
+  </div>
 </template>
 
 <script>
@@ -10,20 +13,20 @@ export default {
     return this.setInitialData();
   },
   computed: {
-    name () {
-      return this.$store.state.name;
+    sku () {
+      return this.$store.state.sku;
     }
   },
   mounted () {
-    if (!this.name) {
+    if (!this.sku) {
       console.log('setting initial data in client');
       this.setInitialData();
     }
   },
   methods: {
     setInitialData () {
-      console.log(this.$route);
-      this.$store.commit('setItem', this.$route.params.name);
+      console.log('setting initial data...');
+      return this.$store.dispatch('fetchItem', this.$route.params.name);
     }
   }
 }
--- src/store.js
+++ src/store.js
@@ -1,31 +1,41 @@
-// store.js
 import { createStore as createVuexStore } from 'vuex'
-
+if (typeof window === 'undefined') {
+  const fetch = require('isomorphic-unfetch')
+}
 // Assume we have a universal API that returns Promises
 // and ignore the implementation details
 // import { fetchItem } from './api'
+
+
 
 export function createStore() {
   return createVuexStore({
     // IMPORTANT: state must be a function so the module can be
     // instantiated multiple times
     state: () => ({
-      name: ''
+      sku: {}
     }),
 
-    // actions: {
-    //   fetchItem({ commit }, id) {
-    //     // return the Promise via `store.dispatch()` so that we know
-    //     // when the data has been fetched
-    //     return fetchItem(id).then(item => {
-    //       commit('setItem', { id, item })
-    //     })
-    //   }
-    // },
+    actions: {
+      fetchItem({ commit }, id) {
+        // return the Promise via `store.dispatch()` so that we know
+        // when the data has been fetched
+        console.log('fetching item');
+        return fetch(`https://www.aceandtate.com/torii/catalog/products?skus=${id}`)
+        .then(r => r.json())
+        .then(item => {
+          console.log(item);
+          commit('setItem', item.data[0]);
+        })
+        .catch(err => {
+          commit('setItem', 'invalid');
+        })
+      }
+    },
 
     mutations: {
       setItem(state, val) {
-        state.name = val;
+        state.sku = val;
       }
     }
   })
